f562dd16198975b5d90645e5229a143fd490168e
jha-pratik <prateekj777@gmail.com>
2020-04-07 01:08:46 +0530

    no refresh of webpage required

--- Definition/background.js
+++ Definition/background.js
@@ -2,15 +2,35 @@
 
 chrome.runtime.onMessage.addListener(receiver);
 
-window.word = 'no text selected';
-
-function receiver(request, sender, sendResponse) {
-
-    //   console.log(request);
-
-    word = request.text;
-}
+chrome.runtime.onInstalled.addListener(installScript);
 
 chrome.commands.onCommand.addListener(function(command) {
     console.log('onCommand event received for message: ', command);
-  });
+});
+
+window.word = 'no text selected';
+
+function receiver(request, sender, sendResponse) {
+    //   console.log(request);
+    word = request.text;
+}
+
+// Installing contentScript in all the tabs 
+function installScript(details){
+    // console.log('Installing content script in all tabs.');
+    let params = {
+        currentWindow: true
+    };
+    chrome.tabs.query(params, function gotTabs(tabs){
+        let contentjsFile = chrome.runtime.getManifest().content_scripts[0].js[0];
+        for (let index = 0; index < tabs.length; index++) {
+            chrome.tabs.executeScript(tabs[index].id, {
+                file: contentjsFile
+            },
+            result => {
+                const lastErr = chrome.runtime.lastError;
+                if (lastErr) console.error('tab: ' + tabs[index].id + ' lastError: ' + JSON.stringify(lastErr));
+            })
+        }
+    });    
+}
